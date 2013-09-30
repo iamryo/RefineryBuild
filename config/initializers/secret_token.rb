78b1d7dc0e557b2684ad37075318585742b80568
Ryan Clark <ryanclark@Ryans-MacBook-Pro.local>
2013-09-30 12:47:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = 'be142ab7ebd9474623e1b1cee054e5a83d09db76dabb6e29d3bf468f955b4041d1b985b95a835c6dea9ce22993fd04c2f8cf2e51f23ecc9f93bac5fbb2a07b24'
